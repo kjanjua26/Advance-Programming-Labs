466563cdccfd0bcbeb9ad2063a99879247738bdb
Kamran Janjua <kamranejaz98@gmail.com>
2018-09-14 18:32:13 +0500

    Update add_sub_mat.cpp

--- MatrixClient_Lab01/add_sub_mat.cpp
+++ MatrixClient_Lab01/add_sub_mat.cpp
@@ -2,22 +2,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<vector<double>> MatrixAdd(vector<vector<double>> mat_a, vector<vector<double>> mat_b, int noRows, int noCol){
-	vector<vector<double>> res;
-	for (int i = 0; i < noRows; i++){
-		for (int j = 0; j < noCol; j++){
-			res[i][j] = mat_a[i][j] + mat_b[i][j];
-		}
-	}
-	return res;
-}
 
-vector<vector<double>> MatrixSub(vector<vector<double>> mat_a, vector<vector<double>> mat_b, int noRows, int noCol){
-	vector<vector<double>> res;
-	for (int i = 0; i < noRows; i++){
-		for (int j = 0; j < noCol; j++){
-			res[i][j] = mat_a[i][j] - mat_b[i][j];
-		}
-	} 
-	return res;
+vector<vector<int> > addVects(vector<vector<int> > A, vector<vector<int> > B){
+    vector<vector<int> > res;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            temp.push_back(A[i][j] + B[i][j]);
+        }
+        res.push_back(temp);
+    }
+    return res;
+}
+vector<vector<int> > subVects(vector<vector<int> > A, vector<vector<int> > B){
+    vector<vector<int> > res;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            temp.push_back(A[i][j] - B[i][j]);
+        }
+        res.push_back(temp);
+    }
+    return res;
 }
