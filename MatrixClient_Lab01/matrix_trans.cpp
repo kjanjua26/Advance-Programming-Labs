baeee5f1cc224e042d126e412c895b14f87bd81d
Kamran Janjua <kamranejaz98@gmail.com>
2018-09-14 18:36:22 +0500

    Update matrix_trans.cpp

--- MatrixClient_Lab01/matrix_trans.cpp
+++ MatrixClient_Lab01/matrix_trans.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 #include "matrixDep.h"
 using namespace std;
-vector<vector<double>>  MatrixT(vector<vector<double>> mat, int noRows, int noCol){
-	for (int i = 0; i < noRows / 2; i++){
-		for (int j = 0; j < noCol / 2; j++){
-			swap(mat[i][j], mat[i][j]);
-		}
-	}
-	return mat;
+vector<vector<int> > MatrixT(vector<vector<int> > A){
+    vector<vector<int> > res;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            temp.push_back(A[j][i]);
+        }
+        res.push_back(temp);
+    }
+    return res;
 }
