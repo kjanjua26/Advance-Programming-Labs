724194eb120b7125ec40ed673ea7b9c8421df9cc
Kamran Janjua <kamranejaz98@gmail.com>
2018-09-14 18:31:42 +0500

    Create main_client.cpp

---
+++ MatrixClient_Lab01/main_client.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include "matAdd.cpp"
+using namespace std;
+int main(){
+    vector<vector<int> > inputMat;
+    vector<vector<int> > addMat;
+    vector<vector<int> > res;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            temp.push_back(i);
+        }
+        inputMat.push_back(temp);
+    }
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            temp.push_back(i);
+        }
+        addMat.push_back(temp);
+    }
+    cout << "Original Matrix 1: " << endl;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            cout << addMat[i][j];
+        }
+        cout << endl;
+    }
+    cout << "Original Matrix 2: " << endl;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            cout << inputMat[i][j];
+        }
+        cout << endl;
+    }
+    res = addVects(inputMat, addMat);
+    cout << "Result of Addition: " << endl;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            cout << res[i][j];
+        }
+        cout << endl;
+    }
+    res = subVects(inputMat, addMat);
+    cout << "Result of Subtraction: " << endl;
+    for(int i = 0; i < 2; i++){
+        vector<int> temp;
+        for(int j = 0; j < 2; j++){
+            cout << res[i][j];
+        }
+        cout << endl;
+    }
+}
